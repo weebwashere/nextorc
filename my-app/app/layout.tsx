377f62b9400ce5db6c107414e9928c04d6c62d1b
weebwashere <128880884+weebwashere@users.noreply.github.com>
2023-07-24 02:01:43 +0000

    tf next is sus

---
+++ my-app/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'weebwashere',
+  description: 'upcoming new next.js landpage for my guilded bot which is based on reminders.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
